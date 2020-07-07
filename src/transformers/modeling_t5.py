990b85e38b74f9ce8dd3f9816d02247a4188b16d
root <you@example.com>
2020-07-07 15:50:51 +0000

    disable loss

--- src/transformers/modeling_t5.py
+++ src/transformers/modeling_t5.py
@@ -1102,7 +1102,8 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
                 DeprecationWarning,
             )
             labels = kwargs.pop("lm_labels")
-        assert kwargs == {}, f"Unexpected keyword arguments: {list(kwargs.keys())}."
+        #assert kwargs == {}, f"Unexpected keyword arguments: {list(kwargs.keys())}."
+        print("Keyword arguments:", list(kwargs.keys()))
 
         use_cache = use_cache if use_cache is not None else self.config.use_cache
 
@@ -1158,14 +1159,16 @@ class T5ForConditionalGeneration(T5PreTrainedModel):
         # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
         sequence_output = sequence_output * (self.model_dim ** -0.5)
         lm_logits = self.lm_head(sequence_output)
-
+        enable_loss = "enable_loss" in kwargs
         decoder_outputs = (lm_logits,) + decoder_outputs[1:]  # Add hidden states and attention if they are here
-        if labels is not None:
+        if enable_loss and labels is not None:
             loss_fct = CrossEntropyLoss(ignore_index=-100)
             loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
             # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
             decoder_outputs = (loss,) + decoder_outputs
 
+        if not enable_loss:
+            return decoder_outputs[0]
         return decoder_outputs + encoder_outputs
 
     def prepare_inputs_for_generation(self, input_ids, past, attention_mask, use_cache, **kwargs):
